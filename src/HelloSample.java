aedbf59f6415b6239e1855b14ca2ca4f84e64eaa
Kostya Plakushchenko <kplakauto@gmail.com>
2019-04-02 10:31:31 +0300

    Adding HelloSample.java

--- src/HelloSample.java
+++ src/HelloSample.java
@@ -2,7 +2,8 @@ public class HelloSample {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World5");
+        System.out.println("Hello World6");
+        
     }
 
 
